cb75f7c5e81129383416686428b59ea055f8c8a8
mahikagup10 <77979481+mahikagup10@users.noreply.github.com>
2023-02-16 12:50:40 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout << "Hi my name is Mahika, SRN:PES1UG20CS243"
+  cout << "Hi my name is Mahika, SRN:PES1UG20CS243";
   return 0;
 }
