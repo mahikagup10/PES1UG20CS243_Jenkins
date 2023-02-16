1c5fc061f029b168998eae598f219cef46a673a9
mahikagup10 <77979481+mahikagup10@users.noreply.github.com>
2023-02-16 09:13:59 +0530

    Create hello.cpp

---
+++ main/hello.cpp
@@ -0,0 +1,11 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run\n";
+  cout << "Hi my name is Mahika, SRN:PES1UG20CS243"
+  return 0;
+}
