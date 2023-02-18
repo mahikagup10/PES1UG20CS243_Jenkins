4562bbf3ad27ade65f09961bac3dc547005ea31e
mahikagup10 <77979481+mahikagup10@users.noreply.github.com>
2023-02-18 22:07:46 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -7,6 +7,6 @@ int main(void) {
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
   cout << "Hi my name is Mahika, SRN:PES1UG20CS243";
-  cout << "This is a new change for the third task";
+  cout << "This is a new change for the 5th task with pipelining";
   return 0;
 }
